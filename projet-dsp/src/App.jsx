01ccf10c5eebbdba08987d4a9f11ef3094b73e51
feyez123456 <feyezbehi@gmail.com>
2023-08-14 16:57:25 +0200

    adding crud to dynamo table by graphql api

--- projet-dsp/src/App.jsx
+++ projet-dsp/src/App.jsx
@@ -1,37 +1,70 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import { API, Amplify, graphqlOperation } from 'aws-amplify';
-import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Amplify, API, graphqlOperation } from 'aws-amplify';
 import { Authenticator } from '@aws-amplify/ui-react';
-import '@aws-amplify/ui-react/styles.css';
 import awsExports from './aws-exports';
-import { listVoyages } from './graphql/queries';
+import {listVoyages} from './graphql/queries'
+import {updateVoyage} from './graphql/mutations'
+import {createVoyage} from './graphql/mutations'
+import {deleteVoyage} from './graphql/mutations'
+
 Amplify.configure(awsExports);
 
-
-
-
-
-// ... TravelEntry, PhotoGallery, and Notes components as before ...
-
 function App() {
-
   const [voyages, setVoyages] = useState([]);
+  const [newVoyagePlace, setNewVoyagePlace] = useState('');
+  const [newVoyageNotes, setNewVoyageNotes] = useState([]);
+  const [newVoyageRating, setNewVoyageRating] = useState(0);
 
   const fetchVoyage = async () => {
     try {
       const voyageData = await API.graphql(graphqlOperation(listVoyages));
       const listVoyage = voyageData.data.listVoyages.items;
-      console.log(listVoyage);
-      setVoyages(voyageData);
+      setVoyages(listVoyage);
     } catch (error) {
-      console.log('error on fetching songs',error);
+      console.log('error on fetching voyages', error);
+    }
+  }
+
+  const addVoyage = async () => {
+    try {
+      const newVoyage = {
+        place: newVoyagePlace,
+        notes: newVoyageNotes,
+        rating: newVoyageRating,
+      };
+      await API.graphql(graphqlOperation(createVoyage, { input: newVoyage }));
+      fetchVoyage();
+      setNewVoyagePlace('');
+      setNewVoyageNotes([]);
+      setNewVoyageRating(0);
+    } catch (error) {
+      console.log('error on adding voyage', error);
+    }
+  }
+
+  const deleteVoyageItem = async (id) => {
+    try {
+      await API.graphql(graphqlOperation(deleteVoyage, { input: { id } }));
+      fetchVoyage();
+    } catch (error) {
+      console.log('error on deleting voyage', error);
+    }
+  }
+
+  const updateVoyageItem = async (id, updatedData) => {
+    try {
+      await API.graphql(graphqlOperation(updateVoyage, { input: { id, ...updatedData } }));
+      fetchVoyage();
+    } catch (error) {
+      console.log('error on updating voyage', error);
     }
   }
 
   useEffect(() => {
     fetchVoyage()
-  }, [])
+  }, []);
+
   return (
     <Authenticator>
       {({ signOut, user }) => (
@@ -42,7 +75,40 @@ function App() {
               <h2>Travel Board</h2>
             </header>
             <div className="voyageList">
-               </div>
+              {voyages.map((voyage) => {
+                return (
+                  <div key={voyage.id} className="voyageItem">
+                    <h1>{voyage.place}</h1>
+                    <p>Notes: {voyage.notes ? voyage.notes.join(', ') : 'N/A'}</p>
+                    <p>Rating: {voyage.rating ? voyage.rating : 'N/A'}</p>
+                    <button onClick={() => deleteVoyageItem(voyage.id)}>Delete</button>
+                    <button onClick={() => updateVoyageItem(voyage.id, { place: 'Updated Place', notes: ['Updated Note'], rating: 5 })}>Update</button>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="addVoyageForm">
+              <h2>Add a New Trip</h2>
+              <input
+                type="text"
+                placeholder="Place"
+                value={newVoyagePlace}
+                onChange={(e) => setNewVoyagePlace(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Notes (comma-separated)"
+                value={newVoyageNotes.join(', ')}
+                onChange={(e) => setNewVoyageNotes(e.target.value.split(', '))}
+              />
+              <input
+                type="number"
+                placeholder="Rating"
+                value={newVoyageRating}
+                onChange={(e) => setNewVoyageRating(parseInt(e.target.value))}
+              />
+              <button onClick={addVoyage}>Add Trip</button>
+            </div>
           </div>
         </main>
       )}
@@ -50,4 +116,4 @@ function App() {
   );
 }
 
-export default withAuthenticator(App);
+export default App;
